f9a395393d525cac8135d65d86b4c1c5eb09eb52
Daniel Gulyas <daniel.gulyas@fashiontrade.com>
2017-10-07 19:51:32 +0200

    Removed storybook too.

--- .storybook/config.js
+++
@@ -1,9 +0,0 @@
-import { configure, addDecorator } from '@storybook/react';
-import 'semantic-ui-css/semantic.min.css';
-
-function loadStories() {
-  const req = require.context('../src/Component', true, /stories.js$/);
-  req.keys().forEach((filename) => req(filename));
-}
-
-configure(loadStories, module);
