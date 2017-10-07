faf7dbff8adc6a3c418530262386592c6e3dfcf7
Daniel Gulyas <daniel.gulyas@fashiontrade.com>
2017-10-07 19:51:02 +0200

    Okie, let's start from the ground-up then.

--- src/Component/Organism/Section/Header/index.stories.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-
-import Header from './';
-
-storiesOf('Organism/Section/Header', Header)
-  .add('default', () => (<Header />));
